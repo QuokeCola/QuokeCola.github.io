157b58608476b296adcd01113633c1e8bda73b3a
quokecola <quokecola@icloud.com>
2022-08-11 03:02:15 +0800

    [Function] Add customized events, make some methods to private.

--- ui_components/article-browser/article_browser_interface.js
+++ ui_components/article-browser/article_browser_interface.js
@@ -6,21 +6,23 @@ class ArticleBrowserIF {
     constructor(markdown_dir_) {
         let _this_ref = this;
 
-        this.md_dir = markdown_dir_;
+        this._md_dir = markdown_dir_;
         this.page_item_number = 6;
 
         let json_request = new XMLHttpRequest();
 
-        this.page_title = document.getElementById("article-title");
-        this.main_content = document.getElementById("article-main-content")
-        this.tag_content = document.getElementById("article-tags-content")
-        this.index_wrapper = document.getElementById("article-index-wrapper")
-        this.index_content = document.getElementById("article-index-content")
+        this._page_title = document.getElementById("article-title");
+        this._main_content = document.getElementById("article-main-content")
+        this._tag_content = document.getElementById("article-tags-content")
+        this._index_wrapper = document.getElementById("article-index-wrapper")
+        this._index_content = document.getElementById("article-index-content")
+        this._recommend_wrapper = document.getElementById("article-recommendations-wrapper")
+        this._recommend_content = document.getElementById("article-recommendations-content")
 
-        this.tag_list = []
-        this.selected_articles = []
+        this._tag_list = []
+        this._selected_articles = []
 
-        json_request.open("get", this.md_dir+"/article_list.json");
+        json_request.open("get", this._md_dir+"/article_list.json");
         json_request.send(null);
 
         json_request.onload = function () {
@@ -28,7 +30,7 @@ class ArticleBrowserIF {
                 let parsed_json = JSON.parse(json_request.responseText)
                 _this_ref.document_info = parsed_json
                 _this_ref.onload();
-                _this_ref.init_tag()
+                _this_ref._init_tag()
             }
         }
     }
@@ -36,7 +38,116 @@ class ArticleBrowserIF {
     onload = function () {
     }
 
-    generate_link_card(article_list_object) {
+    /**
+     * Load article page by page number from article list.
+     * @param page page to show.
+     */
+    show_page(page) {
+        this._page_title.innerText="Articles"
+        for(let i = (page-1) * this.page_item_number; i<((page*this.page_item_number < this._selected_articles.length)?
+            (page*this.page_item_number):this._selected_articles.length); i++){
+            try{
+                this._main_content.append(this._generate_link_card(this._selected_articles[i]));
+            } catch (e) {
+                console.log(e);
+            }
+        }
+    }
+
+    /**
+     * Load article by markdown file url
+     * @param url Markdown file url.
+     */
+    show_article(url) {
+        let _this_ref = this
+        let md_file = new XMLHttpRequest()
+        let markdown_container = document.createElement("div")
+        md_file.open("get", this._md_dir+ "/" + url)
+        md_file.send(null)
+        md_file.onload = function () {
+            if (md_file.status === 200) {
+                markdown_container.innerHTML =  marked.parse(md_file.responseText)
+                _this_ref._main_content.append(markdown_container)
+                hljs.highlightAll();
+            }
+        }
+    }
+
+    /**
+     * Update the article list with selected tags
+     * @param tags String array with tags.
+     */
+    refresh_article_list(tags) {
+        this._selected_articles = []
+        if (tags === []) { /// Empty indicates all tags
+            this._selected_articles = this.document_info
+        } else {           /// Else, tags selected.
+            this.document_info.forEach(article_obj=>{
+                let satisfied_tag_search = true
+                tags.forEach(tag=> {
+                    if (!article_obj.class.includes(tag)) {
+                        satisfied_tag_search = false
+                    }
+                })
+                if (satisfied_tag_search) {
+                    this._selected_articles.push(article_obj)
+                }
+            })
+        }
+    }
+
+    /**
+     * Update the indexes button with list.
+     */
+    refresh_indexes() {
+        let _this_ref = this
+        this._index_content.innerHTML=""
+        for(let i = 0; i < Math.ceil(this._selected_articles.length/this.page_item_number); i++) {
+            let btn = document.createElement("button")
+            btn.innerText = (i+1).toString()
+            btn.onclick = function () {
+                let page_btn_click_event = new CustomEvent("article_page_refresh_event", {
+                    detail: (i+1)
+                });
+                window.dispatchEvent(page_btn_click_event)
+            }
+            this._index_content.appendChild(btn)
+        }
+    }
+
+    clear_main_view() {
+        this._main_content.innerHTML = ""
+    }
+
+    /**
+     * Animation will take 250ms
+     */
+    content_panel_fade() {
+        this._main_content.style.opacity = "0.0"
+    }
+
+    /**
+     * Animation will take 250ms
+     */
+    content_panel_show() {
+        this._main_content.style.opacity = "1.0"
+    }
+
+    /**
+     * Animation will take 250ms
+     */
+    index_panel_fade() {
+        this._index_wrapper.style.opacity = "0.0"
+    }
+
+    /**
+     * Animation will take 250ms
+     */
+    index_panel_show() {
+        this._index_wrapper.style.opacity = "1.0"
+    }
+
+    _generate_link_card(article_list_object) {
         let card_obj = document.createElement("div")
         let img_wrapper = document.createElement("div")
         let img = document.createElement("img")
@@ -55,7 +166,7 @@ class ArticleBrowserIF {
         link_img.classList.add("article-card-link-arrow")
         link_img.src = "/resources/htmls/home/linkArrow.svg"
 
-        img.src = this.md_dir + "/" + article_list_object.pic
+        img.src = this._md_dir + "/" + article_list_object.pic
         img_wrapper.append(img);
         card_obj.append(img_wrapper)
         card_obj.append(title)
@@ -65,80 +176,9 @@ class ArticleBrowserIF {
         return card_obj;
     }
 
-    /**
-     * Update the article list with selected tags
-     * @param tags String array with tags.
-     */
-    update_article_list(tags) {
-        this.selected_articles = []
-        if (tags === []) { /// Empty indicates all tags
-            this.selected_articles = this.document_info
-        } else {           /// Else, tags selected.
-            this.document_info.forEach(article_obj=>{
-                let satisfied_tag_search = true
-                tags.forEach(tag=> {
-                    if (!article_obj.class.includes(tag)) {
-                        satisfied_tag_search = false
-                    }
-                })
-                if (satisfied_tag_search) {
-                    this.selected_articles.push(article_obj)
-                }
-            })
-        }
-    }
-
-    /**
-     * Load article page by page number from article list.
-     * @param page page to show.
-     */
-    show_page(page) {
-        this.page_title.innerText="Articles"
-        for(let i = (page-1) * this.page_item_number; i<((page*this.page_item_number < this.selected_articles.length)?
-            (page*this.page_item_number):this.selected_articles.length); i++){
-            try{
-                this.main_content.append(this.generate_link_card(this.selected_articles[i]));
-            } catch (e) {
-                console.log(e);
-            }
-        }
-    }
-
-    /**
-     * Load article by markdown file url
-     * @param url Markdown file url.
-     */
-    show_article(url) {
-        let _this_ref = this
-        let md_file = new XMLHttpRequest()
-        let markdown_container = document.createElement("div")
-        md_file.open("get", this.md_dir+ "/" + url)
-        md_file.send(null)
-        md_file.onload = function () {
-            if (md_file.status === 200) {
-                markdown_container.innerHTML =  marked.parse(md_file.responseText)
-                _this_ref.main_content.append(markdown_container)
-                hljs.highlightAll();
-            }
-        }
-    }
-
-    refresh_indexes() {
-        let _this_ref = this
-        this.index_content.innerHTML=""
-        for(let i = 0; i < Math.ceil(this.selected_articles.length/this.page_item_number); i++) {
-            let btn = document.createElement("button")
-            btn.innerText = (i+1).toString()
-            btn.onclick = function () {
-                let page_btn_click_event = new CustomEvent("article_page_refresh_event");
-                page_btn_click_event.detail = (i+1)
-            }
-            this.index_content.appendChild(btn)
-        }
-    }
-
-    init_tag() {
+    _init_tag() {
         let added_tags = []
+        let _this_ref = this
         this.document_info.forEach(article_obj=>{
             article_obj.class.forEach(tag_str=>{
                 if(!added_tags.includes(tag_str)) {
@@ -146,7 +186,7 @@ class ArticleBrowserIF {
                     let tag_obj = new Object()
                     tag_obj.tag = tag_str
                     tag_obj.selected = false
-                    this.tag_list.push(tag_obj)
+                    this._tag_list.push(tag_obj)
 
                     let tag_btn = document.createElement("button")
                     tag_btn.innerText = tag_str
@@ -157,39 +197,20 @@ class ArticleBrowserIF {
                         } else {
                             tag_btn.classList.add("article-tag-selected")
                         }
-                        let tag_btn_click_event = new CustomEvent("article_page_refresh_event");
+                        let selected_tags = []
+                        _this_ref._tag_list.forEach(tag=>{
+                            if (tag.selected) {
+                                selected_tags.push(tag.tag)
+                            }
+                        })
+                        let tag_btn_click_event = new CustomEvent("article_page_refresh_event", {
+                            detail: {selected_tags}
+                        });
+                        window.dispatchEvent(tag_btn_click_event)
                     }
-                    this.tag_content.appendChild(tag_btn)
+                    this._tag_content.appendChild(tag_btn)
                 }
             })
         })
-    }
-
-    /**
-     * Animation will take 250ms
-     */
-    content_panel_fade() {
-        this.main_content.style.opacity = "0.0"
-    }
-
-    /**
-     * Animation will take 250ms
-     */
-    content_panel_show() {
-        this.main_content.style.opacity = "1.0"
-    }
-
-    /**
-     * Animation will take 250ms
-     */
-    index_panel_fade() {
-        this.index_wrapper.style.opacity = "0.0"
-    }
-
-    /**
-     * Animation will take 250ms
-     */
-    index_panel_show() {
-        this.index_wrapper.style.opacity = "1.0"
     }
 }
